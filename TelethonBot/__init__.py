30b89712246fbc69512daaa6d3b78f23d1d187e5
Shashank Sharma <78837537+theshashankk@users.noreply.github.com>
2023-01-11 00:14:49 +0530

    Update __init__.py

--- TelethonBot/__init__.py
+++ TelethonBot/__init__.py
@@ -2,6 +2,7 @@
 # // @BotzHub //
 
 from telethon import TelegramClient
+from telethon.sessions import StringSession
 from decouple import config
 import logging
 import time
@@ -10,8 +11,8 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # Basics
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
+API_ID = 9397588
+API_HASH = "79d67b413fdc8065c9f914e05d83bd11"
+SESSION = """1AZWarzYBu4qYMFR0BrdJzuNh2uIoSOG0OguCahSAzPuQsdfSrWUiMANNOBI0yhVvXYDm3IEq1ffQSsKaBIh0CbM18sxUBvhX8pwuejF9N3FGu_Yv0IBdWj0cWRthQOSysgDTZdvYS9FstXv1_rvrFHBI2XlyTq-dnidjiEkoOA_Kp-VU9LnWHjIUrX6QOIi7RMCT9G2p4CFivihq1UWzrX-3qj8_vSkbCYu04a92sa1Qoi1p9akF0rPUuzmoBgNNsVaC9WtkJxo6dZyzNqwMkTklbr1fmAUH486kmjJpVyL4c56M8uG1riuwH273KAOW-ju7tKsp5HYZmtVXSmL1tYG-y3GjGws="""
 
-BotzHub = TelegramClient('BotzHub', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
+BotzHub = TelegramClient(StringSession(SESSION), API_ID, API_HASH))
