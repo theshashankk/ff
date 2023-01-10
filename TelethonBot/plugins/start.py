b82b596697da5def4ffd47242fd4eb1146ae178c
Shashank Sharma <78837537+theshashankk@users.noreply.github.com>
2023-01-11 00:15:41 +0530

    Update start.py

--- TelethonBot/plugins/start.py
+++ TelethonBot/plugins/start.py
@@ -5,11 +5,7 @@ from telethon import events, Button
 
 @BotzHub.on(events.NewMessage(incoming=True, pattern="/start"))
 async def start(event):
-    await event.reply("Hello!",
-                    buttons=[
-                        [Button.url("ButtonUrl", url="https://t.me/xditya")],
-                        [Button.inline("Inline Button",data="example")]
-                    ])
+    await event.reply("Heelo")
 
 @BotzHub.on(events.callbackquery.CallbackQuery(data="example"))
 async def ex(event):
